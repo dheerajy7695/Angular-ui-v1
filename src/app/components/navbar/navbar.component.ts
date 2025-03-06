671d429377efed71e9b8004c031bd368676c2def
dheerajy7695 <dheerajy906@gmail.com>
2025-03-06 15:46:49 +0530

    updated navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  tokenloginData: any;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    let loginData = sessionStorage.getItem('login-user');
+    if (loginData != 'undefined') {
+      this.tokenloginData = JSON.parse(loginData || '{}');
+    } else {
+      this.tokenloginData = '';
+    }
+  }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('login-user');
+    this.router.navigate(['/login'])
+  }
 
 }
